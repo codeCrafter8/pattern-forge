008461d054be8b0a92327d84346ea31524cc1773
zuzannawozniak <zuzanna.wk@interia.pl>
2025-03-23 15:21:39 +0100

    Initialize project structure

---
+++ backend/src/main/java/com/example/patternforge/PatternForgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.patternforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternForgeApplication.class, args);
+	}
+
+}
