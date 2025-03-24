adeb45e9b54083b445ad30cc2257f8515c78da01
zuzannawozniak <zuzanna.wk@interia.pl>
2025-03-24 12:42:56 +0100

    Add config button

--- frontend/src/app/features/config-panel/config-panel.component.ts
+++ frontend/src/app/features/config-panel/config-panel.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-config-panel',
   imports: [
     MatCardModule,
     ReactiveFormsModule,
+    MatButtonModule,
   ],
   templateUrl: './config-panel.component.html',
   styleUrl: './config-panel.component.scss'
