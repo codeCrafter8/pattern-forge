66c817a10b2825c413a6f4aec14bb9f1d5d8ca31
zuzannawozniak <zuzanna.wk@interia.pl>
2025-05-11 15:23:47 +0200

    Fix test

--- frontend/src/app/services/zip-download.service.spec.ts
+++ frontend/src/app/services/zip-download.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ZipDownloadService } from './zip-download.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ZipDownloadService', () => {
   let service: ZipDownloadService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+    });
     service = TestBed.inject(ZipDownloadService);
   });
 
