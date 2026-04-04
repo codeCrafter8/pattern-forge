4974462d147f56f24c872082a986d542214bc81e
zuzannawozniak <zuzanna.wk@interia.pl>
2026-04-04 18:21:17 +0200

    Fix tests

--- frontend/src/app/components/ai-generator-dialog/ai-generator-dialog.component.spec.ts
+++ frontend/src/app/components/ai-generator-dialog/ai-generator-dialog.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AiGeneratorDialogComponent } from './ai-generator-dialog.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AiCodeGeneratorService } from '../../services/ai-code-generator.service';
+import { of } from 'rxjs';
 
 describe('AiGeneratorDialogComponent', () => {
   let component: AiGeneratorDialogComponent;
@@ -8,7 +10,11 @@ describe('AiGeneratorDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AiGeneratorDialogComponent]
+      imports: [AiGeneratorDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: { close: () => {} } },
+        { provide: AiCodeGeneratorService, useValue: { generate: () => of({}) } }
+      ]
     })
     .compileComponents();
 
--- frontend/src/app/services/ai-code-generator.service.spec.ts
+++ frontend/src/app/services/ai-code-generator.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AiCodeGeneratorService } from './ai-code-generator.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AiCodeGeneratorService', () => {
   let service: AiCodeGeneratorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+      });
     service = TestBed.inject(AiCodeGeneratorService);
   });
 
